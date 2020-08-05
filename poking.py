7c5a6d7a5ff3d4efeeb8e3a8e941e8bf640c880a
edwarddavis1 <edwarddavis@hotmail.co.uk>
2020-08-05 17:41:55 +0100

    Re-written the code with a class structure

--- poking.py
+++ poking.py
@@ -1,4 +1,14 @@
 import numpy as np
-x = np.array([1, 2, 3, 4, 5])
-x = np.delete(x, 2)
+
+x = np.array([10, 20, 30, 40, 50])
+i = 0
+for v in x:
+    print("%s, %s" % (i, v))
+    if v in (10, 30, 40):
+        x = np.delete(x, i)
+    else:
+        i += 1
+x
+x
+x = np.delete(x, 1)
 x
